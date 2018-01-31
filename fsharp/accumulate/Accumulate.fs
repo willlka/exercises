3603e72eb5d3bfe8bccd09324d77e5db295a3253
willlka <yana.ufa@gmail.com>
2018-01-31 14:02:14 +0500

    Accumulate 2nd iteration

--- fsharp/accumulate/Accumulate.fs
+++ fsharp/accumulate/Accumulate.fs
@@ -1,9 +1,8 @@
 ﻿module Accumulate
 
-let accumulate<'a, 'b> (func: 'a -> 'b) (input: 'a list): 'b list = 
-    let result = []
-    let rec funcRec (tail: 'a list, i: int): 'b list =  
-        match tail with
-        | [] -> result
-        | _ -> result |> List.append (funcRec (tail.Tail, i+1)) |> List.append [func(tail.Head)]
-    funcRec(input, 0)
+//not my solution, but it's perfect, I submit it just because I don't want to forget it
+let rec accumulate<'a, 'b> (func: 'a -> 'b) (input: 'a list): 'b list = 
+    match input with
+    | [] -> []
+    | h::t -> func(h) :: (accumulate func t)
+    
